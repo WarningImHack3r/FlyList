d85cd8c4908fd587376375faf85e94e85b2bd02d
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2021-03-18 21:28:20 +0100

    check for internet connection

--- app/src/main/java/com/antoine/flylist/MainActivity.kt
+++ app/src/main/java/com/antoine/flylist/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.antoine.flylist
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -13,6 +16,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.antoine.flylist.data.Flight
 import com.antoine.flylist.data.FlightAPI
+import com.antoine.flylist.io.CheckNetwork
 import com.antoine.flylist.list.FlightsAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -38,10 +42,15 @@ class MainActivity : AppCompatActivity() {
         findViewById<ProgressBar>(R.id.loading_circle).visibility = View.GONE
         findViewById<TextView>(R.id.loading_text).visibility = View.GONE
         findViewById<TextView>(R.id.error_label).visibility = View.GONE
+        findViewById<TextView>(R.id.no_connection_label).visibility = View.GONE
 
         findViewById<FloatingActionButton>(R.id.floating_button).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+
+        if (Build.VERSION.SDK_INT >= 24) {
+            CheckNetwork(applicationContext).registerNetworkCallback()
         }
 
         // API section
@@ -85,18 +94,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateRecyclerViewWithAPICall(call: Call<List<Flight>>) {
-        // Managing UI
+        if (call != lastCall) lastCall = call
+        // Empty Recycler View
         if (!flightsAdapter.isEmpty()) flightsAdapter.updateList(arrayOf())
+        // Check for connection
+        val errorText = findViewById<TextView>(R.id.error_label)
+        errorText.visibility = View.GONE
+        val noInternet = findViewById<TextView>(R.id.no_connection_label)
+        if ((Build.VERSION.SDK_INT >= 24 && CheckNetwork.hasConnection) || (Build.VERSION.SDK_INT < 24 && (applicationContext.getSystemService(
+                Context.CONNECTIVITY_SERVICE
+            ) as ConnectivityManager).activeNetworkInfo?.isConnected == true)
+        ) {
+            noInternet.visibility = View.GONE
+        } else {
+            noInternet.visibility = View.VISIBLE
+            return
+        }
+        // Managing UI
         val circle = findViewById<ProgressBar>(R.id.loading_circle)
         circle.visibility = View.VISIBLE
         val circleText = findViewById<TextView>(R.id.loading_text)
         circleText.visibility = View.VISIBLE
-        val errorText = findViewById<TextView>(R.id.error_label)
-        errorText.visibility = View.GONE
         // Debug log
         Logger.getGlobal().info(call.request().url().toString())
         // Calling
-        lastCall = call
         call.clone().enqueue(object : Callback<List<Flight>> {
             override fun onResponse(call: Call<List<Flight>>, response: Response<List<Flight>>) {
                 if (response.isSuccessful && response.body() != null) {
---
+++ app/src/main/java/com/antoine/flylist/io/CheckNetwork.kt
@@ -0,0 +1,37 @@
+package com.antoine.flylist.io
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.ConnectivityManager.NetworkCallback
+import android.net.Network
+import android.net.NetworkRequest
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+
+class CheckNetwork(private val context: Context) {
+    companion object {
+        var hasConnection = false
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun registerNetworkCallback() {
+        try {
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            NetworkRequest.Builder()
+            connectivityManager.registerDefaultNetworkCallback(object : NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    hasConnection = true
+                }
+
+                override fun onLost(network: Network) {
+                    hasConnection = false
+                }
+            })
+            hasConnection = false
+        } catch (e: Exception) {
+            hasConnection = false
+        }
+    }
+}
